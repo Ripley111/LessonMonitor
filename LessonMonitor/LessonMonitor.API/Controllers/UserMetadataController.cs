630861a813a0f99c70adcb1282331b0607d682bb
Ripley111 <ox1om2002@gmail.com>
2025-04-30 16:07:26 +0300

    Реализовал новый неймспейс, а также новый контроллер с методом, который возвращает метаданные классов в этом неймспейсе.

---
+++ LessonMonitor/LessonMonitor.API/Controllers/UserMetadataController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace LessonMonitor.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserMetadataController : ControllerBase
+    {
+        [HttpGet("userModelInfo", Name = "GetUserModelInfo")]
+        public IActionResult GetModel()
+        {
+            string targetNamespace = "LessonMonitor.API.Models";
+            var classes = Assembly.GetExecutingAssembly()
+                                  .GetTypes()
+                                  .Where(x => x.Namespace == targetNamespace)
+                                  .ToList();
+
+            ClassMetadata metadataInfo = new ClassMetadata();
+            foreach (var classType in classes)
+            {
+                var modelName = classType.Name;
+                metadataInfo.ClassName = modelName;
+
+                var modelPropetries = classType.GetProperties();
+
+                foreach (var property in modelPropetries)
+                {
+                    metadataInfo.PropertyInfo.Add(new PropertyMetadata
+                    {
+                        PropertyName = property.Name,
+                        PropertyDescription = property.GetCustomAttribute<DescriptionAttribute>()?.Description ?? string.Empty,
+                        PropetryType = property.PropertyType.Name
+                    });
+                }
+            }
+            return Ok(metadataInfo);
+        }
+    }
+
+    public record ClassMetadata
+    {
+        public string ClassName { get; set; }
+
+        public List<PropertyMetadata> PropertyInfo { get; set; } = new List<PropertyMetadata>();
+    }
+
+    public record PropertyMetadata
+    {
+        public string PropertyName { get; set; }
+
+        public string PropertyDescription { get; set; }
+
+        public string PropetryType { get; set; }
+    }
+}
---
+++ LessonMonitor/LessonMonitor.API/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace LessonMonitor.API.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+
+        [Description("Имя")]
+        public string FirstName { get; set; }
+
+        [Description("Фамилия")]
+        public string SecondName { get; set; }
+
+        [Description("Электронный адрес")]
+        public string Email { get; set; }
+    }
+}
